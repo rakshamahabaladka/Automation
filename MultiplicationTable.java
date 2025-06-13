9ec2b42b842c58622a297da9c79ba055c7506620
rakshamahabaladka <89260424+rakshamahabaladka@users.noreply.github.com>
2025-06-13 20:41:59 +0530

    Add files via upload

---
+++ MultiplicationTable.java
@@ -0,0 +1,13 @@
+package loopingstatements;
+
+public class MultiplicationTable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  int num = 5;
+	        for(int i=1;i<=10;i++) {
+	        	System.out.println(num+ " * "+ i +" = "+ num*i);
+	        }
+	}
+
+}
